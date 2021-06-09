74261d1222c197e1f371c919fd6b612a6c8b14ea
AdrianDucao <adrianducao@gmail.com>
2021-06-09 11:10:30 +0800

    added function -writing history activity -checking history activity file -showing history activity

--- scan.py
+++ scan.py
@@ -10,21 +10,66 @@ import csv
 
 from datetime import datetime
 
+def check_history():
+    try:
+        with open('history.csv','rb') as f:
+            reader=csv.reader(f)
+    except:
+         with open('history.csv','w') as csvfile:
+             filewriter = csv.writer(csvfile, delimiter=',', 
+                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
+             currentDate = datetime.now()
+             filewriter.writerow([currentDate])
+
+def show_history():
+    try:
+        with open('history.csv') as f:
+            reader = csv.reader(f)
+
+            for row in reader:
+                print(row)
+    except:
+        pass
+def write_history(action, data):
+    with open('history.csv','a', newline='') as csvfile: 
+        filewriter = csv.writer(csvfile, delimiter=',', 
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        currentDate = datetime.now()
+        data1 = action
+        data2 = data
+        filewriter.writerow([data1, data2])
 
 def socket_Scan(targetIP):
+    def write(action, data):
+        with open('history.csv','a', newline='') as csvfile:
+             filewriter = csv.writer(csvfile, delimiter=',', 
+                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
+             currentDate = datetime.now()
+             data1 = action
+             data2 = data
+             filewriter.writerow([data1, data2])
+
+    dt = datetime.now()
+    timer = time.time()
     host = gethostbyname(targetIP)
-    print('Scanning...',host)
-    
-    
+    action = '-------------------Port Scan--------------------- \n'
+    data = 'Scanning...'
+    write(action,data)
+
+    print('Scanning...')
+
     for i in range(0, 65535):
         soc = socket(AF_INET, SOCK_STREAM)
         connect = soc.connect_ex((host, i))
         
         if(connect == 0):
-            print('Port %d: [OPEN]'%(i,))
+            print('Port %d: ->[OPEN]'%(i,))
+            data = 'Port %d: ->[OPEN]}'%(i,)
             soc.close()
-
-    print('time taken = ', time.time() - timer)
+            write('', data)
+    time_taken = time.time() - timer
+    write('time taken =',time_taken)
+    print('time taken = ', time_taken)
 
 def ping_Sweep(job_q, results_q):
     DEVNULL = open(os.devnull, 'w')    
@@ -39,18 +84,6 @@ def ping_Sweep(job_q, results_q):
             results_q.put(ip)
         except:
             pass
-
-
-def history():
-    try:
-        with open('history.csv', 'rb') as f:
-            reader = csv.reader(f)
-    except:
-        with open('history.csv', 'wb') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            filewriter.writerow(['Action Performed', 'Date/Time', 'Data'])
-            filewriter.writerow(['pressed 3','06/09/2021','192.168.254.105 --> is Live'])
 
 
 if __name__ == '__main__':    
@@ -70,10 +103,15 @@ if __name__ == '__main__':
 
     if(action == '1'):
         targetIP = input('===[Scan Open Ports]=== \n Enter IP: ')
-        timer = time.time()
+        check_history()
         socket_Scan(targetIP)
     
     elif(action == '2'):
+        check_history()
+        action = '-------------------Ping Sweep Live Host--------------------- \n'
+        data = 'Scanning'
+        write_history(action, data)
+
         size = int(input('===[Ping Sweep Live Host]=== \n Enter Range(1 to ?): '))
         targetNetwork = input('\n Enter Network IP: ')
         splitIP = targetNetwork.split('.')
@@ -98,6 +136,9 @@ if __name__ == '__main__':
 
         while not results.empty():
             liveIP = results.get()
+            data = liveIP,'---> we got a live one!'
+            dateTime = datetime.now()
+            write_history(dateTime, data)
             print(liveIP, '---> we got a live one!')
             
 
@@ -105,10 +146,9 @@ if __name__ == '__main__':
         print('will add soon...')
     
     elif(action == '4'):
+        show_history()
+
+    elif(action == '5'):
         print('will add soon...')
-        history()
-    elif(action == '5')
-        
-    
     else:
         print('incorrect option...')
